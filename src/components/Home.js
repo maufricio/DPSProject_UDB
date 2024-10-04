23ffec7426db22b2de9608cb67aac53dd2b26fa4
i-ashy <ashleymelinares@gmail.com>
2024-10-03 23:10:39 -0600

    Movilizando Dashboard

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,11 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text } from 'react-native';
+import * as React from 'react';
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
+import Schedule from '../components/Schedule.js';
+import Dashboard from '../components/Dashboard.js';
 
 export default function Home() {
+
   return (
     <View>
       <Text>Home</Text>
